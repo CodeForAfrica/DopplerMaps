869876309cf61808671ca17bce566d9b5ea943c2
Chafic Najjar <chafic.najjar@gmail.com>
2017-01-12 15:15:27 +0200

    Implement responsive legend.

--- js/doppler-maps.js
+++ js/doppler-maps.js
@@ -23,13 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let width = 800, height = 600;
     let maps = document.querySelectorAll('.doppler-maps');
     [...maps].forEach((mapEl) => {
-        let map = d3.select(mapEl)
-            .style('width', '100%')
-            .append('canvas')
-            .attr('width', width)
-            .attr('height', height);
-
-        let context = map.node().getContext('2d');
+        let map = d3.select(mapEl);
 
         let parseData = (d) => {
             let values = [];
@@ -55,6 +49,73 @@ document.addEventListener('DOMContentLoaded', () => {
             .await((error, geodata, data) => {
                 if (error) throw error;
 
+                // Create color scale.
+                let interpolator = d3.interpolateRgb(mapEl.dataset.colorLowest, mapEl.dataset.colorHighest);
+                let colorPalette = d3.quantize(interpolator, mapEl.dataset.legendNumberOfColors);
+                let max = d3.max(data, d => d3.max(d.values, d => d.value));
+                let min = d3.min(data, d => d3.min(d.values, d => d.value));
+                let colorScale = d3.scaleQuantize()
+                    .domain([min, max])
+                    .range(colorPalette);
+
+                // Add legend container.
+                let mapLegend = map.append('div')
+                    .attr('class', 'doppler-maps__legend')
+                    .style('text-align', 'center');
+
+                // Add title only if the user specified one.
+                if (mapEl.dataset.legendTitle !== '' && typeof mapEl.dataset.legendTitle !== 'undefined') {
+                    mapLegend.append('div')
+                        .attr('class', 'doppler-maps__legend-title')
+                        .style('display', 'inline-block')
+                        .html(mapEl.dataset.legendTitle)
+                        .append('br');
+                }
+
+                // Add legend color palette items.
+                const TICK_WIDTH = 1;
+                let legendColorPaletteItems = mapLegend.append('div')
+                    .attr('class', 'doppler-maps__legend-color-palette')
+                    .selectAll('.doppler-maps__legend-color-palette-item')
+                    .data(colorScale.range())
+                  .enter().append('div')
+                    .attr('class', 'doppler-maps__legend-color-palette-item')
+                    .style('position', 'relative')
+                    .style('display', 'inline-block')
+                    .style('margin-right', TICK_WIDTH + 'px')
+                    .style('max-width', '50px')
+                    .style('width', 'calc(100% / 6 - ' + TICK_WIDTH + 'px)')
+                    .style('height', '10px')
+                    .style('background-color', d => d);
+
+                // Add legend color palette ticks.
+                legendColorPaletteItems.filter(':not(:last-child)')
+                    .append('div')
+                    .attr('class', 'doppler-maps__legend-color-palette-tick')
+                    .style('position', 'absolute')
+                    .style('top', '0')
+                    .style('right', -TICK_WIDTH + 'px')
+                    .style('display', 'inline-block')
+                    .style('width', TICK_WIDTH + 'px')
+                    .style('height', '100%')
+                    .style('background-color', '#333')
+                    .style('font-size', '14px')
+                  .append('div')
+                    .style('position', 'absolute')
+                    .style('left', '-1em')
+                    .style('margin-top', '18px')
+                    .style('width', '2em')
+                    .style('line-height', '1em')
+                    .text(d => d3.format('.1f')(colorScale.invertExtent(d)[1]));
+
+                // Create HTML5 Canvas element.
+                let canvas = map.style('width', '100%')
+                    .append('canvas')
+                    .attr('width', width)
+                    .attr('height', height);
+
+                let context = canvas.node().getContext('2d');
+
                 // Sort data.
                 data.sort((a, b) => compareWords(a['Geographic Administrative Unit'], b['Geographic Administrative Unit']));
 
@@ -66,15 +127,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     .projection(createProjection(geodata, width, height))
                     .context(context);
 
-                let max = d3.max(data, d => d3.max(d.values, d => d.value));
-                let min = d3.min(data, d => d3.min(d.values, d => d.value));
-                let color = d3.scaleLinear()
-                    .domain([min, max])
-                    .range(['yellow', 'red']);
-
                 let geographicAdministrativeUnits = topojson.feature(geodata, geodata.objects['us-states']).features;
                 geographicAdministrativeUnits.forEach((geographicAdministrativeUnit, i) => {
-                    context.fillStyle = color(data[i].values[0].value);
+                    context.fillStyle = colorScale(data[i].values[0].value);
                     context.beginPath();
                     path(geographicAdministrativeUnit);
                     context.stroke();
